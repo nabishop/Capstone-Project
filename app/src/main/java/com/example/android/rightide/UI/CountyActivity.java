090646a2a3c3561e720cae7fab89a35888835915
nabishop <35745821+nabishop@users.noreply.github.com>
2018-09-24 15:56:31 -0700

    set up menu and layout/java placeholders

--- app/src/main/java/com/example/android/rightide/UI/CountyActivity.java
+++ app/src/main/java/com/example/android/rightide/UI/CountyActivity.java
@@ -1,4 +1,29 @@
 package com.example.android.rightide.UI;
 
-public class CountyActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.android.rightide.R;
+
+public class CountyActivity extends AppCompatActivity {
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.county_activity);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        mOnNavigationItemSelectedListener = NavigationBar.createNavigationBar();
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        setUpCountyFragment();
+    }
+
+    public void setUpCountyFragment() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.county_activity_county_fragment, new CountyFragment(), null).commit();
+    }
 }
--- app/src/main/java/com/example/android/rightide/UI/CountyFragment.java
+++ app/src/main/java/com/example/android/rightide/UI/CountyFragment.java
@@ -1,4 +1,22 @@
 package com.example.android.rightide.UI;
 
-public class CountyFragment {
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.rightide.R;
+
+public class CountyFragment extends Fragment {
+    public CountyFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.county_fragment, container, false);
+        return root;
+    }
 }
