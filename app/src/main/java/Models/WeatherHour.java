03588169a9dffe7cb4fc46a93c45bb408c5d4694
nabishop <35745821+nabishop@users.noreply.github.com>
2018-09-24 19:03:05 -0700

    maybe finished models for now

--- app/src/main/java/Models/WeatherHour.java
+++ app/src/main/java/Models/WeatherHour.java
@@ -1,10 +1,76 @@
 package Models;
 
-public class WeatherHour {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class WeatherHour implements Parcelable {
     private String swellRating;
     private String tideRating;
-    private String wideRating;
+    private String windRating;
     private String waveSizeFeet;
     private String hour;
 
+    public WeatherHour(String swellRating, String tideRating, String windRating,
+                       String waveSizeFeet, String hour) {
+        this.swellRating = swellRating;
+        this.tideRating = tideRating;
+        this.windRating = windRating;
+        this.waveSizeFeet = waveSizeFeet;
+        this.hour = hour;
+    }
+
+
+    protected WeatherHour(Parcel in) {
+        swellRating = in.readString();
+        tideRating = in.readString();
+        windRating = in.readString();
+        waveSizeFeet = in.readString();
+        hour = in.readString();
+    }
+
+    public static final Creator<WeatherHour> CREATOR = new Creator<WeatherHour>() {
+        @Override
+        public WeatherHour createFromParcel(Parcel in) {
+            return new WeatherHour(in);
+        }
+
+        @Override
+        public WeatherHour[] newArray(int size) {
+            return new WeatherHour[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(swellRating);
+        dest.writeString(tideRating);
+        dest.writeString(windRating);
+        dest.writeString(waveSizeFeet);
+        dest.writeString(hour);
+    }
+
+    public String getHour() {
+        return hour;
+    }
+
+    public String getSwellRating() {
+        return swellRating;
+    }
+
+    public String getTideRating() {
+        return tideRating;
+    }
+
+    public String getWaveSizeFeet() {
+        return waveSizeFeet;
+    }
+
+    public String getWindRating() {
+        return windRating;
+    }
 }
--- app/src/main/java/Models/Wind.java
+++ app/src/main/java/Models/Wind.java
@@ -5,4 +5,29 @@ public class Wind {
     private String windDirectionCompass;
     private String hour;
     private double windSpeedMph;
+
+    public Wind(String windDirectionDegrees, String windDirectionCompass, String hour,
+                double windSpeedMph) {
+        this.windDirectionDegrees = windDirectionDegrees;
+        this.windDirectionCompass = windDirectionCompass;
+        this.hour = hour;
+        this.windSpeedMph = windSpeedMph;
+    }
+
+    public double getWindSpeedMph() {
+        return windSpeedMph;
+    }
+
+    public String getHour() {
+        return hour;
+    }
+
+    public String getWindDirectionCompass() {
+        return windDirectionCompass;
+    }
+
+    public String getWindDirectionDegrees() {
+        return windDirectionDegrees;
+    }
+
 }
